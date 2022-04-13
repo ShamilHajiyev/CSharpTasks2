30d05d702c2224f669107bba68f528afcb99b5a9
ShamilHajiyev <shamilnh@code.edu.az>
2022-04-13 15:13:10 +0400

    created two tasks

---
+++ Comparison/Comparison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir eded daxil edin:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+
+                Console.WriteLine("Daxil etdiyiniz eded cüt ededdir.");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Daxil etdiyiniz eded tek ededdir.");
+
+            }
+        }
+    }
+}
---
+++ Odd or even/Odd or even/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Odd_or_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir eded daxil edin:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Başqa bir eded daxil edin:");
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+
+                Console.WriteLine(num1 + " daha böyükdür.");
+
+            }
+
+            else if (num1 < num2)
+            {
+
+                Console.WriteLine(num2 + " daha böyükdür.");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Daxil etdiyiniz ededler beraberdir.");
+            }
+        }
+    }
+}
