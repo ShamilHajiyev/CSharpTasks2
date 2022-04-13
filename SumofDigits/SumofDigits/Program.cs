61d0abf561781609b845069eec8ea02950fc4765
ShamilHajiyev <shamilnh@code.edu.az>
2022-04-14 02:00:46 +0400

    initial commit

---
+++ SumofDigits/SumofDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumofDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir eded daxil edin:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (n != 0)
+            {
+                sum = n % 10 + sum;
+                n = n / 10;
+            }
+            Console.WriteLine("\nEdedin reqemlerinin cemi:\n" + sum);
+        }
+    }
+}
